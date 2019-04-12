be22f325604a1b31d80b20be4e298a67872eb127
kshammer <hammerschmiks@vcu.edu>
2019-04-11 22:43:12 -0500

    fixed PriorityQueue implementation and added some tests for it

--- spooti/PriorityQueue.py
+++ spooti/PriorityQueue.py
@@ -1,35 +1,41 @@
+import sys
 class PriorityQueue(object): 
     def __init__(self): 
         self.queue = []
         self.base = set()
+        self.smallest = sys.maxsize
 
     def __str__(self): 
         return ' '.join([str(i) for i in self.queue]) 
   
     # for checking if the queue is empty 
     def isEmpty(self): 
-        return len(self.queue) == []
+        return len(self.queue) == 0
+
+    def clear(self):
+        self.queue = []
+        self.base = set()
+        self.smallest = sys.maxsize
     
     def setBase(self, base):
         self.base = set(base)
   
     # inserts an element based on how different it is from the base set 
-    def insert(self, data):
+    def insert(self, data, artist_id):
         data_set = set(data)
         intersection = self.base.intersection(data_set)
         diff = abs(len(self.base) - len(intersection)) # absolute value of the difference between the two sets 
-        data_set_diff = (data_set, diff)
-        if self.isEmpty():
-            self.queue.append((data_set, diff)) # tuple of information and the difference from the base 
-
-        # for the love of god someone help me flatten this function out 
-        else:
-            for elements in self.queue:
-                if diff <= elements[1]:
-                    self.queue.insert(self.queue.index(elements), data_set_diff)
-                    break
-        if data_set_diff not in set(self.queue):
-            self.queue.append(data_set_diff)        
-   
+        if diff <= self.smallest:
+            self.smallest = diff
+        everything = (data, artist_id, diff)
+        self.queue.append(everything)
+          
     def pop(self):
-        return self.queue.pop(0)(0)
+        next_smallest = list(filter(lambda x: x[2] <= self.smallest, self.queue))
+        while(len(next_smallest) == 0): # basically if the smallest ele is taken then it can't find the next smallest. This method is dumb and needs to be refactored 
+            self.smallest += 1
+            next_smallest = list(filter(lambda x: x[2] <= self.smallest, self.queue))
+        output = next_smallest.pop(0)
+        # have to remove because of 2 separate lists 
+        self.queue.remove(output)
+        return output
