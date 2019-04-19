b93665597d4044f372ccfee81ad735eaf0088dbe
kshammer <hammerschmiks@vcu.edu>
2019-04-18 22:44:18 -0500

    trying again.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(
     name='Spooti',
     version='0.1dev',
-    packages=['spooti', 'spotipy'], #at some point we need to change this to just auto discover packages
+    packages=['spooti',],
+    install_requires=['spotipy'], #at some point we need to change this to just auto discover packages
 )
