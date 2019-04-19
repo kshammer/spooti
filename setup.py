2a2627302ea07e4aa7e0a0f1e2d3387a7a0f6844
kshammer <hammerschmiks@vcu.edu>
2019-04-18 22:41:39 -0500

    fixed for build

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name='Spooti',
     version='0.1dev',
-    packages=['spooti',],
+    packages=['spooti', 'spotipy'], #at some point we need to change this to just auto discover packages
 )
